08c7c7e5096209aab1d0faf9e2def8691e9a05d1
LOctavio <luis_ram1997@hotmail.com>
2022-03-15 16:03:09 -0600

    Add search input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { FormControl, InputGroup, Button } from 'react-bootstrap';
 
 function App() {
+  const [definition, setDefinition] = useState();
+
+  const getDefinition = async (text) => {
+    const response = await axios(`https://api.dictionaryapi.dev/api/v2/entries/en/${text}`);
+    setDefinition(response.data);
+    console.log(definition);
+  };
+
   return (
-    <h1>hello world</h1>
+    <>
+      <div className="main-search-section">
+        <h1>Search a word to find the meaning</h1>
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Type word"
+            aria-label="Word"
+            aria-describedby="basic-addon2"
+          />
+          <Button variant="outline-secondary" id="button-addon2" onClick={(e) => getDefinition(e.target.previousElementSibling.value)}>
+            Search
+          </Button>
+        </InputGroup>
+      </div>
+    </>
   );
 }
 
