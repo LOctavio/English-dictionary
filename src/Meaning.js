5805f84fb526bf5bda51bee023e435201b1ca2a7
LOctavio <luis_ram1997@hotmail.com>
2022-03-16 14:06:25 -0600

    Fetch data from API when rendering view

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,9 +1,34 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './App.css';
 
 function Meaning() {
   const { slug } = useParams();
   const word = slug.split(' ').join('_').toLowerCase();
+  let wordData = {};
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
+      ).then((response) => {
+        const { data } = response;
+        wordData = data;
+      }).catch((error) => {
+        // Error
+        if (error.response) {
+          wordData = { error: 'We have no registers about this word' };
+        } else {
+          console.log('Error', error.message);
+        }
+      });
+      console.log(wordData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="main-search-section">
       <h1>{word}</h1>
