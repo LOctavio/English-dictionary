7c7ae37718e704ed6b5caa102ed3d69722493ad8
LOctavio <luis_ram1997@hotmail.com>
2022-03-16 12:11:13 -0600

    Get the slug from the url

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,9 +1,12 @@
+import { useParams } from 'react-router-dom';
 import './App.css';
 
 function Meaning() {
+  const { slug } = useParams();
+  const word = slug.split(' ').join('_').toLowerCase();
   return (
     <div className="main-search-section">
-      <h1>hi</h1>
+      <h1>{word}</h1>
     </div>
   );
 }
