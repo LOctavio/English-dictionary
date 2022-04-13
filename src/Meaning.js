fb1de86a6260b412a0274e29654eb71728dadb7b
LOctavio <luis_ram1997@hotmail.com>
2022-04-13 18:04:56 -0500

    Store api data in state

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,37 +1,39 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './App.css';
 
 function Meaning() {
   const { slug } = useParams();
   const word = slug.split(' ').join('_').toLowerCase();
-  let wordData = {};
+  const [wordData, setWordData] = useState([{ message: 'Loading data' }]);
+
+  const fetchData = async () => {
+    const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      .catch(() => console.log('error'));
+    const { data } = response || { data: [{ message: 'Not found' }] };
+    console.log(data);
+    setWordData(data);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      await axios.get(
-        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
-      ).then((response) => {
-        const { data } = response;
-        wordData = data;
-      }).catch((error) => {
-        // Error
-        if (error.response) {
-          wordData = { error: 'We have no registers about this word' };
-        } else {
-          console.log('Error', error.message);
-        }
-      });
-      console.log(wordData);
-    };
-
     fetchData();
   }, []);
 
+  const pageData = () => {
+    if (wordData[0].word) {
+      return (
+        <div>
+          <h1>{wordData[0].word}</h1>
+        </div>
+      );
+    }
+    return (<h1>{wordData[0].message}</h1>);
+  };
+
   return (
-    <div className="main-search-section">
-      <h1>{word}</h1>
+    <div>
+      {pageData()}
     </div>
   );
 }
